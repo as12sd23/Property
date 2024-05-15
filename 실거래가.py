a08cc3f0534db69d3ad55b1baaa98f60ff3b1075
as12sd23 <52819784+as12sd23@users.noreply.github.com>
2024-05-15 19:51:16 +0900

    Add files via upload

--- 실거래가.py
+++ 실거래가.py
@@ -1,9 +1,24 @@
 import requests
 import pandas as pd
 import xml.etree.ElementTree as ET
+import os
+import time
 
+# 아파트 실거래가 검색
+def getRtData(LAWD_CD, DEAL_YMD):
+    #LAWD_CD는  지역번호 (천안시 서북그 : 44133)
+    #DEAL_YMD는 날짜
+    key = 'moueEHwKWvcAlOWx8KbuFkzCwihNexLOFDAFsd%2B5kvMf%2B7OeeC8%2BvhCBZ2UV8MdSZTGf9VsngtjPvjNLcwHTlg%3D%3D'
+    url = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?"
+    
+    payload = "LAWD_CD=" + str(LAWD_CD) + "&" + \
+              "DEAL_YMD=" + str(DEAL_YMD) + "&" + \
+              "serviceKey=" + key + "&"
 
-def get_items(response):
+    return requests.get(url + payload)
+
+# xml 해석
+def get_item(response):
     root = ET.fromstring(response.content)
     item_list = []
     for child in root.find('body').find('items'):
@@ -13,23 +28,45 @@ def get_items(response):
             tag = element.tag.strip()
             text = element.text.strip()
             data[tag] = text
-        item_list.append(data)  
+        item_list.append(data)
     return item_list
 
-key = 'OADc2WewAVKTkOGZ%2BLNIqcVFTPXDRYkFpRvF2j4QmNmlN8I84zpQYsY%2BO8Y3D3ozVXrQvC%2B1owwXV6K%2FhhB%2Bpg%3D%3D'
-url = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?"
-code_file = "C:\Users\c404\Desktop\sangjin\실거래가"
-base_date = "202001"
-gu_code = '44133'
+# 지역번호 추출
+def get_LAND_CD(LAND_CD):
+    codefile = 'C:/Users/c404/Desktop/sangjin/exchange/code.txt'
+    code = pd.read_csv(codefile, sep='\t', encoding='cp949')
+    code.columns = ['code', 'name', 'is_exist']
+    code = code[code['is_exist'] == '존재']
+    code['code'] = code['code'].apply(str)
+    
+    gu_code = code[ (code['name'].str.contains(LAND_CD) )]
+    gu_code = gu_code['code'].reset_index(drop=True)
+    gu_code = str(gu_code[0])[0:5]
+    return gu_code
 
-payload = "LAWD_CD=" + gu_code + "&" + \
-          "DEAL_YMD=" + base_date + "&" + \
-          "serviceKey=" + key + "&"
+# 날짜 선택
+def get_date(StartTime, EndTime):
 
-res = requests.get(url + payload)
+    year = [str("%02d" %(y)) for y in range(int(StartTime), int(EndTime) + 1)]
+    month = [str("%02d" %(m)) for m in range(1,13)]
+    
+    datetime  = ["%s%s" %(y,m) for y in year for m in month]
+    return datetime
 
-print(res)
 
-item = get_items(res)
-items = pd.DataFrame(item)
+Land = input("지역 검색\n > ")
+Lands = get_LAND_CD(Land)
+
+StartTime = input("시작년도 입력(4자리)\n  예시) 2020\n > ")
+EndTime= input ("종료년도 입력(4자리)\n  예시) 2024\n > ")
+Time = get_date(StartTime, EndTime)
+
+item_list = []
+for i in Time:
+    Response = getRtData(Lands, Time)
+    item_list += get_item(Response)
+items = pd.DataFrame(item_list)
 items.head()
+print(item_list)
+items.to_csv(os.path.join("%s-%s~%s.csv" %(Land, Time[0], Time[1])), index = False, encoding='euc-kr')
+
